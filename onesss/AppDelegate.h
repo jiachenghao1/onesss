a603b6630cdcb2236971ff39de34665d27978e14
JCHSS <136691850@qq.com>
2016-04-27 13:33:30 +0800

    ggggg
    hhhh

---
+++ onesss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  onesss
+//
+//  Created by MS on 16-4-27.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
